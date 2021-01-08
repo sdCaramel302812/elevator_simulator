237d8d17bfdf2b8bb352fdd4b2e66cd502fea329
sdCaramel302812 <kypss93097@gmail.com>
2021-01-08 21:31:00 +0800

    Add floor component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="top-block">
+      <b-row>
+        <b-col cols="3">
+          <b-button variant="success" class="start-button">start</b-button>
+        </b-col>
+        <b-col cols="5">
+          <h1 id="title">Elevator Simulator</h1>
+        </b-col>
+        <b-col cols="3">
+          <h3 id="title">wait time: </h3>
+        </b-col>
+      </b-row>
+    </div>
+    <TopFloor 
+      :peopleNumber="0"
+      :floorNumber="16"
+     />
+     <div v-for="n in 15" :key="n">
+       <MiddleFloor
+        :peopleNumber="0"
+        :floorNumber="16 - n"
+       />
+     </div>
+     <BottomFloor
+      :peopleNumber="0"
+      :floorNumber="-1"
+     />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopFloor from './components/TopFloor.vue';
+import MiddleFloor from './components/MiddleFloor.vue';
+import BottomFloor from './components/BottomFloor.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    TopFloor,
+    MiddleFloor,
+    BottomFloor,
+  },
+  data() {
+    return {
+      floorPeopleCount: [],
+    };
+  },
 }
 </script>
 
@@ -21,8 +55,23 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#top-block {
+  width: 90%;
+  min-width: 1000px;
+  height: 60px;
+  margin: auto;
+}
+
+#title {
+  text-align: center;
+}
+
+.start-button {
+  height: 48px;
+  width: 80px;
 }
 </style>
---
+++ src/components/TopFloor.vue
@@ -0,0 +1,78 @@
+<template>
+  <div id="floor-block">
+    <div id="left-outline"></div>
+    <div id ="right-outline">
+      <b-img src="../assets/People-05.svg" class="people-icon" />
+      <span class="number">{{ peopleNumber }}</span>
+      <span class="floor-number">{{ floorNumber }}F</span>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TopFloor',
+  data() {
+    return {
+    };
+  },
+  props: {
+    peopleNumber: Number,
+    floorNumber: Number,
+  },
+};
+</script>
+<style scoped>
+#floor-block {
+  width: 90%;
+  min-width: 1000px;
+  height: 120px;
+  margin: auto;
+  margin-bottom: 30px;
+}
+
+#left-outline {
+  width: 50%;
+  height: 100%;
+  border-style: ridge;
+  border-width: 2px;
+  border-color: gray;
+  border-right: transparent;
+  border-bottom: transparent;
+  border-top-left-radius: 20px;
+  border-bottom-left-radius: 20px;
+  float: left;
+}
+
+#right-outline {
+  width: 50%;
+  height: 100%;
+  border-style: ridge;
+  border-width: 2px;
+  border-color: gray;
+  border-left: transparent;
+  border-top-right-radius: 20px;
+  border-bottom-right-radius: 20px;
+  float: left;
+  padding: 20px;
+}
+
+.people-icon {
+  text-align: left;
+  width: 80px;
+  height: 80px;
+  vertical-align: baseline;
+  margin-right: 20px;
+  float: left;
+}
+
+.number {
+  line-height: 1;
+  font-size: 80px;
+}
+
+.floor-number {
+  line-height: 1;
+  font-size: 80px;
+  float: right;
+}
+</style>
